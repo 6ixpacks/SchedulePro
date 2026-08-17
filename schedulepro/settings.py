c2de3c9f8f2ea967180e1220405dd0e75b1aa498
6ixpacks <davidukanwoke@gmail.com>
2026-08-17 10:09:09 +0100

    Adjusted the setting

--- schedulepro/settings.py
+++ schedulepro/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
